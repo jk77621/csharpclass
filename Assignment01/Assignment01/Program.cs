540c2facacd5ce5da539368e495c03763df45bcc
jk77621 <jk77621@naver.com>
2022-07-19 01:43:56 +0900

    assignment01
    assignment01

---
+++ Assignment01/Assignment01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
