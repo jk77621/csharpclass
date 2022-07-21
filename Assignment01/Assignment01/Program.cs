d58d5d261fe19f64b835edc0fa208ad1be0064a3
jk77621 <jk77621@naver.com>
2022-07-21 11:00:31 +0900

    assignment03
    2022-07-21 4type change

--- Assignment01/Assignment01/Program.cs
+++ Assignment01/Assignment01/Program.cs
@@ -10,29 +10,43 @@ namespace Assignment01
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World");
-            // Console.ReadKey();
+            /*
+            Console.WriteLine("Hello World");
+            */
 
+            /*
             string string1;
             string string2;
-            char string3;
-            int number1;
+            */
+            string string3;
+            string string4;
+            int int1;
+            float float1;
 
-            Console.Write("Enter String1: ");
+            /*
+            Console.Write("Write String1: ");
             string1 = Console.ReadLine();
 
-            Console.Write("Enter String2: ");
+            Console.Write("Write String2: ");
             string2 = Console.ReadLine();
 
             Console.WriteLine("1. " + string1 + "\n" + string2);
             Console.WriteLine("2. " + $"{string1}\n{string2}");
             Console.WriteLine("3. {1}\n{0}", string2, string1);
-            Console.WriteLine($"4. " + string1 + "\n" + string2);
+            Console.WriteLine(@"4. " + string1 + "\n" + string2);
+            */
 
-            Console.WriteLine("Enter Number1: ");
-            number1 = Console.Read();
-            string3 = (char)number1;
-            Console.Write(string3);
+            Console.Write("Write Number: ");
+            string3 = Console.ReadLine();
+
+            int1 = int.Parse(string3);
+            Console.WriteLine("int : " + int1);
+
+            float1 = (float)int1;
+            Console.WriteLine("float : " + float1);
+
+            string4 = float1.ToString();
+            Console.WriteLine("string : " + string4);
 
             Console.ReadKey();
         }
