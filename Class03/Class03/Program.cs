44c8b21fab6d538663a03d68f36f33d66f4851b6
jk77621 <67351218+jk77621@users.noreply.github.com>
2022-07-31 11:43:08 +0900

    class03
    2022-07-31 simple test

---
+++ Class03/Class03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Class03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hello world");
+            WriteLine();
+            WriteLine();
+
+            int number = 3;
+            string word = "Hi";
+
+            WriteLine("1. " + number +  word);
+            WriteLine("2. " + $"{number}{word}");
+            WriteLine("3. {0}{1}", number, word);
+            ReadKey();
+        }
+    }
+}
+
+/* 생성자
+ * 가비지 콜렉터가 없기 때문에 생성자 사용x, 소멸자 사용x
+ */
+
+
